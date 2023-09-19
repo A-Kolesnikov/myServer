3ce6929217d739a44b6c2eaaeb5e20fd05bee9f8
A-Kolesnikov <kolesnikov.spb@gmail.com>
2023-09-19 08:10:03 +0300

    implemented basic commands for cart

--- routes/products.js
+++ routes/products.js
@@ -2,14 +2,14 @@ var express = require('express')
 var router = express.Router()
 
 const {getCategoryByID, addCategory} = require('../data_managers/categoriesManager')
-const {getProduct, getProducts, getProductsOfCategory} = require('../data_managers/productsManager')
+const {getProduct, getProducts, getProductsWithIDs, getProductsOfCategory} = require('../data_managers/productsManager')
 
 router.get('/', async function (req, res, next) {
     try {
         const products = await getProducts()
         res.status(200).json(products)
-    } catch (error) {
-        res.status(500).json({ error: "an error happened" })
+    } catch (err) {
+        res.status(500).json({ error: err })
     }
 })
 
@@ -19,9 +19,23 @@ router.get('/product/:id', async function (req, res, next) {
     res.status(200).json(product)
 })
 
+router.get('/with-ids/:ids', async function (req, res, next) {
+    const ids = req.params.ids
+    try{
+        const products = await getProductsWithIDs(ids)
+        res.status(200).json(products)
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+})
+
 router.get('/of-category/:id', async function (req, res, next) {
     const id = req.params.id
-    const products = await getProductsOfCategory(id)
-    res.status(200).json(products)
+    try {
+        const products = await getProductsOfCategory(id)
+        res.status(200).json(products)
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
 })
 module.exports = router
