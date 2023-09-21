3a5fd285bd7be01874f8c16b635892ba95012b9c
A-Kolesnikov <kolesnikov.spb@gmail.com>
2023-09-21 10:32:46 +0300

    implemented fetch by tag

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 const {getCategoryByID, addCategory} = require('../data_managers/categoriesManager')
-const {getProduct, getProducts, getProductsWithIDs, getProductsOfCategory} = require('../data_managers/productsManager')
+const {getProduct, getProducts, getProductsWithIDs, getProductsOfCategory, getProductsByTag} = require('../data_managers/productsManager')
 
 router.get('/', async function (req, res, next) {
     try {
@@ -38,4 +38,15 @@ router.get('/of-category/:id', async function (req, res, next) {
         res.status(500).json({ error: err })
     }
 })
+
+router.get('/by-tag/:tag', async function (req, res, next) {
+    const tag = req.params.tag
+    try {
+        const products = await getProductsByTag(tag)
+        res.status(200).json(products)
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+})
+
 module.exports = router
